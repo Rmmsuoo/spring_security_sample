958853e0922f9a5acc83c74d952c1804f35ff4a5
Noboru Shimomura <rmmsuoorbnauoi@gmail.com>
2023-09-13 18:13:18 +0900

    lesson03追加

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/loginForm").permitAll()
+				.anyRequest().authenticated();
 		http.formLogin()
 				.loginProcessingUrl("/login")
 				.loginPage("/loginForm")
