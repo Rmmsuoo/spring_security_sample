2c404ec926257617bbd09d339feb1a23bbef8cb9
Noboru Shimomura <rmmsuoorbnauoi@gmail.com>
2023-09-14 03:49:05 +0900

    lesson05実装完了

--- src/main/java/com/example/service/LoginUser.java
+++ src/main/java/com/example/service/LoginUser.java
@@ -32,7 +32,10 @@ public class LoginUser implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return AuthorityUtils.NO_AUTHORITIES;
+		if (this.user.getRole().equals("管理者")) {
+			return AuthorityUtils.createAuthorityList("ADMIN", "GENERAL");
+		}
+		return AuthorityUtils.createAuthorityList("GENERAL");
 	}
 
 	@Override
